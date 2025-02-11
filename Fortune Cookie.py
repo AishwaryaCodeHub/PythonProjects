5ecce329217c3bc19e07c6fe88d30ba8085f66a9
AishwaryaCodeHub <aishwaryan0305@gmail.com>
2025-02-11 15:32:26 +0530

    Add files via upload

---
+++ Fortune Cookie.py
@@ -0,0 +1,14 @@
+import random
+
+num = random.randint(1,5)
+
+if num == 1:
+    print(f"Your greatest dream is not far. Prepare to embrace it.\nYour Fortune number is {num}")
+elif num == 2:
+    print(f"An unexpected journey will lead to wonderful surprises.\nYour Fortune number is {num}")
+elif num == 3:
+    print(f"Courage is not the absence of fear, but the conquest of it.\nYour Fortune number is {num}")
+elif num == 4:
+    print(f"A joyful heart is inevitable result of a heart burning with love.\nYour Fortune number is {num}")
+else:
+    print(f"Paitence is your alley at the moment. Good things come to those who wait.\nYour Fortune number is {num}")
